2226073debab5e99a40594ba2edf43c3a1c1af0f
Ubuntu <albert.gejr@gmail.com>
2021-07-29 10:43:24 +0000

    Cleaned some code

--- app.py
+++ app.py
@@ -3,29 +3,22 @@ import json
 import requests
 #from flask import Flask
 
-def get_default_branch(url, default_branch):
-    headers = {
-        "ref": "refs/heads/" + default_branch
-    }
-    branch = requests.get(url,headers=headers)
-    return branch
-
 def main():
     GITHUB_EVENT_PATH = os.getenv("GITHUB_EVENT_PATH")
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
     GITHUB_SHA = os.getenv("GITHUB_SHA")
 
-    print("sha = ", GITHUB_SHA)
+    print("sha =", GITHUB_SHA)
 
     data = {}
 
-    print(GITHUB_EVENT_PATH)
+    print("Event path =",GITHUB_EVENT_PATH)
     if GITHUB_EVENT_PATH:
         with open(str(GITHUB_EVENT_PATH), 'r') as json_file:
             data = json.load(json_file)
     
-    print(data)
-    print(GITHUB_TOKEN)
+    print("Data =",data)
+    print("Token =",GITHUB_TOKEN)
     if data and GITHUB_TOKEN:
         username = data["sender"]["login"]
         token = GITHUB_TOKEN
@@ -35,20 +28,15 @@ def main():
         url = "https://api.github.com/" + "repos/" + username + "/" + repo + "git/refs"
 
         login = requests.get("https://api.github.com/search/repositories?q=github+api", auth=(username,token))
-        print(login)
+        print("Login status =", login)
 
-        default_branch = get_default_branch(url, data["repository"]["default_branch"])
-        print(default_branch)
-
-        if default_branch:
-            default_branch_sha = default_branch["object"]["sha"]
-
+        if GITHUB_SHA:
             headers = {
                 "ref": ref,
-                "sha": default_branch_sha
+                "sha": GITHUB_SHA
             }
             create_branch = requests.post(url, headers=headers)
-            print(create_branch)
+            print("Create branch status =",create_branch)
 
 if __name__ == "__main__":
     main()
