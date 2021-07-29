ad6d36ba6cb68415724bcf2fff3469014f3df439
Ubuntu <albert.gejr@gmail.com>
2021-07-29 10:33:12 +0000

    Added branch making.. maybe

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import json
 import requests
 #from flask import Flask
 
+def get_default_branch(url, default_branch):
+    headers = {
+        "ref": "refs/heads/" + default_branch
+    }
+    branch = requests.get(url,headers=headers)
+    return branch
+
 def main():
     GITHUB_EVENT_PATH = os.getenv("GITHUB_EVENT_PATH")
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -19,8 +26,24 @@ def main():
     if data and GITHUB_TOKEN:
         username = data["sender"]["login"]
         token = GITHUB_TOKEN
+        repo = data["repository"]["name"]
+        ref = "refs/heads/" + data["issue"]["title"].replace(" ","_")
+
+        url = "https://api.github.com/" + "repos/" + username + "/" + repo + "git/refs"
+
         login = requests.get("https://api.github.com/search/repositories?q=github+api", auth=(username,token))
         print(login)
+
+        default_branch = get_default_branch(url, data["repository"]["default_branch"])
+        print(default_branch)
+        default_branch_sha = default_branch["object"]["sha"]
+
+        headers = {
+            "ref": ref,
+            "sha": default_branch_sha
+        }
+        create_branch = requests.post(url, headers=headers)
+        print(create_branch)
 
 if __name__ == "__main__":
     main()
