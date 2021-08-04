3dcd73661bfe8b74e199bdf2d4ce1114f8e1a225
Ubuntu <albert.gejr@gmail.com>
2021-08-04 09:56:17 +0000

    Refactored some code and added some TODOs

--- processor/issue_event_processor.py
+++ processor/issue_event_processor.py
@@ -1,6 +1,14 @@
 import os
 import requests
 
+def create_issue_comment(comment_body, repo, repo_owner, issue_number, auth_header):
+    parameters = {
+        "issue_number": issue_number,
+        "body": comment_body
+    }
+    url = "https://api.github.com/repos/" + repo_owner + "/" + repo + "/issues/" + str(issue_number) + "/comments"
+    return requests.post(url=url,json=parameters,headers=auth_header)
+    
 def create_branch_from_default_branch(repo_owner, repo, issue_number, issue_title, auth_header):
     # TODO: Add check to see if branch already exists
 
@@ -19,35 +27,36 @@ def create_branch_from_default_branch(repo_owner, repo, issue_number, issue_titl
 
         print("Creating branch...")
 
-        create_branch = requests.post(url=url, json=parameters, headers=auth_header)
+        branch_status = requests.post(url=url, json=parameters, headers=auth_header)
 
-        print("Create branch status =",create_branch)
+        print("Create branch status (keys, test feature) =",branch_status.keys()) # TODO: remove testfeature
 
     else:
         if not GITHUB_TOKEN:
             raise Exception("ERROR: no Github token")
         elif not GITHUB_SHA:
             raise Exception("ERROR GITHUB_SHA is null")
+        #elif erroneus statuscode
 
     comment_body = "Branch [" + branch_name + "](https://github.com/" + repo_owner + "/" + repo + "/tree/" + branch_name + ") created!"
-    parameters = {
-        "issue_number": issue_number,
-        "body": comment_body
-    }
-    url = "https://api.github.com/repos/" + repo_owner + "/" + repo + "/issues/" + str(issue_number) + "/comments"
-    print("Creating comment on issue...")
-    create_comment = requests.post(url=url,json=parameters,headers=auth_header)
-    print("Create comment status = ", create_comment)
+    comment_status = create_issue_comment(comment_body, repo, repo_owner, issue_number, auth_header)
+    print("Create comment status = ", comment_status)
+    
+# TODO: def add_issue_to_board(...)
+# When a new issue is created, add it to the backlog in the Kanban board 
 
 def process_issue(event_data, auth_header):
+    repo_owner = event_data["repository"]["owner"]["login"]
+    repo = event_data["repository"]["name"]
+    issue_number = event_data["issue"]["number"]
+    issue_title = event_data["issue"]["title"]
 
     # If the event is an issue event where someone has been assigned
     # Then create a new issue-named branch
     if event_data["action"] == "assigned":
-        repo_owner = event_data["repository"]["owner"]["login"]
-        repo = event_data["repository"]["name"]
-        issue_number = event_data["issue"]["number"]
-        issue_title = event_data["issue"]["title"]
+        
         create_branch_from_default_branch(repo_owner, repo, issue_number, issue_title, auth_header) 
+
+    # TODO: If the event is an issue that has just been created call add_issue_to_board 
 
     
