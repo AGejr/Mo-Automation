e00d6b0a6fc4669d69bbe5aa9c2070be6759db5b
Ubuntu <albert.gejr@gmail.com>
2021-08-06 12:31:03 +0000

    Removed dependency

--- vars/env.py
+++ vars/env.py
@@ -1,21 +1,21 @@
-from os import getenv
+import os
 
 class ENV_VAR:
     __conf = {
-        "GITHUB_EVENT_PATH" : getenv("GITHUB_EVENT_PATH"),
-        "GITHUB_API_URL" : getenv("GITHUB_API_URL"),
+        "GITHUB_EVENT_PATH" : os.getenv("GITHUB_EVENT_PATH"),
+        "GITHUB_API_URL" : os.getenv("GITHUB_API_URL"),
 
-        "GITHUB_TOKEN" : getenv("GITHUB_TOKEN"),
-        "AUTH_HEADER" : {"Authorization":"Token " + getenv("GITHUB_TOKEN")},
+        "GITHUB_TOKEN" : os.getenv("GITHUB_TOKEN"),
+        "AUTH_HEADER" : {"Authorization":"Token " + os.getenv("GITHUB_TOKEN")},
         "ALTERNATE_AUTH_HEADER" : {
             "Accept":"application/vnd.github.inertia-preview+json",
-            "Authorization":"Token " + getenv("GITHUB_TOKEN")
+            "Authorization":"Token " + os.getenv("GITHUB_TOKEN")
         },
-        "GITHUB_SHA" : getenv("GITHUB_SHA"),
-        "PROJECT_BOARD_NAME" : getenv("PROJECT_BOARD_NAME", "Automated Board"),
-        "GITHUB_REPOSITORY_OWNER" : getenv("GITHUB_REPOSITORY").split("/")[0],
-        "GITHUB_REPOSITORY_TITLE" : getenv("GITHUB_REPOSITORY").split("/")[1],
-        "GITHUB_ACTOR" : getenv("GITHUB_ACTOR")
+        "GITHUB_SHA" : os.getenv("GITHUB_SHA"),
+        "PROJECT_BOARD_NAME" : os.getenv("PROJECT_BOARD_NAME", "Automated Board"),
+        "GITHUB_REPOSITORY_OWNER" : os.getenv("GITHUB_REPOSITORY").split("/")[0],
+        "GITHUB_REPOSITORY_TITLE" : os.getenv("GITHUB_REPOSITORY").split("/")[1],
+        "GITHUB_ACTOR" : os.getenv("GITHUB_ACTOR")
     }
 
     @staticmethod
